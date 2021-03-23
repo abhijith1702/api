24d209b327f6085bbbfa9896fff777f7bcc105bf
abhijith1702 <47925746+abhijith1702@users.noreply.github.com>
2021-03-23 13:29:43 -0500

    Delete src directory

--- src/main/java/dev/microservice/api/repository/PalindromeRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.microservice.api.repository;
-
-import dev.microservice.api.model.Palindrome;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PalindromeRepository extends CrudRepository<Palindrome,Long> {
-}
